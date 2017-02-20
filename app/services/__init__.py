3480cd2e5a4b32e472b7bfa71610303df1d2bb6d
Ming <ming.george.li@gmail.com>
2017-02-21 05:41:33 +0800

    restructure

---
+++ app/services/__init__.py
@@ -0,0 +1,16 @@
+from .. import settings
+from ..map_urls import sorted_urls
+from ..helpers import NHSTextMiner, NLPProcessor
+
+TEXTMINER=settings.TEXTMINER
+NHS_BASE_URL=settings.NHS_BASE_URL
+NLP_PROCESSOR=settings.NLP_PROCESSOR
+API_BASE_URL = settings.API_BASE_URL
+
+web_pages = sorted_urls(base_url=NHS_BASE_URL)
+web_scraper = NHSTextMiner(urls=web_pages, attrs=TEXTMINER, display=True)
+raw_data = web_scraper.extract()
+
+labels = {key: raw_data[key][0] for key in raw_data}
+mapping = {v: k for k, v in labels.items()}
+
---
+++ app/settings.py
@@ -0,0 +1,30 @@
+# setting file
+
+"""keys to detect useful information and main article of web pages."""
+
+NHS_BASE_URL = 'http://www.nhs.uk'
+API_BASE_URL = '/chatbot/api/v1'
+DATA_LOC = 'app/cache/'
+
+TEXTMINER = {
+    'desc_attributes': {
+        'name': 'description'
+    },
+    'subj_attributes': {
+        'name': 'DC.title'
+    },
+    'article_attributes': {
+        'start_t_0': 'Overview',
+        'start_t_1': 'Print this page',
+        'start_t_2': 'Print this page',
+        'end_t_0': 'Share:',
+        'end_t_1': '',
+        'end_t_2': ''
+    }
+}
+
+NLP_PROCESSOR = {
+        'pipeline': {'pos': True, 'stop': True, 'lemma': True},
+        'part_of_speech_include': {'ADJ', 'DET', 'ADV', 'ADP', 'VERB', 'NOUN', 'PART'}
+        # 'part_of_speech_include': {'ADJ', 'DET', 'ADV', 'SPACE', 'CONJ', 'PRON', 'ADP', 'VERB', 'NOUN', 'PART'}
+}
