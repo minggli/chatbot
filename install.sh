768b3d87580d25aba629263f93a43f39b8a50f17
Ming <ming.george.li@gmail.com>
2017-03-10 15:35:11 +0000

    edit install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,4 @@
 pip3 install virtualenv
 virtualenv venv
-source venv/bin/activate
-pip3 install -r requirements.txt
-source venv/bin/activate
-python3 -m spacy.en.download all
-source venv/bin/activate
+venv/bin/pip3 install -r requirements.txt
+venv/bin/python3 -m spacy.en.download all
