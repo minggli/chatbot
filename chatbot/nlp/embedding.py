679f8f8f24f2e8b49da93f9903a78ece3484294c
Ming Li <ming.george.li@gmail.com>
2017-06-07 10:49:46 +0100

    improve efficiency of embedding module

--- chatbot/nlp/embedding.py
+++ chatbot/nlp/embedding.py
@@ -14,37 +14,34 @@ import spacy
 import collections
 
 
-class WordVectorizer:
+class VectorLookup:
 
     def __init__(self):
         self._nlp = spacy.load('en_core_web_md')
 
     @property
-    def __corpus(self):
+    def __corpus__(self):
         return self._corpus
 
-    @__corpus.setter
-    def __corpus(self, value):
+    @__corpus__.setter
+    def __corpus__(self, value):
+        """index 0 reserved for out-of-vocabulary."""
         if isinstance(value, collections.Iterable) and \
                 not any(not isinstance(item, str) for item in value):
-            self._corpus = value
-            self._cleanse()
+            tokens = [word for word in value].insert(0, ' ')
+            self._corpus = self(' '.join(tokens))
         else:
-            raise TypeError('corpus must be a list-like iterable of strings.')
+            raise TypeError('corpus must be a iterable containing strings.')
 
     def __call__(self, texts):
         return self._nlp(texts)
 
-    def _cleanse(self):
-        """replace non-ascii characters with a single space"""
-        self._corpus = [''.join([i if (i.isalpha() or ord(i) == 39)
-                        else ' ' for i in string]) for string in self._corpus]
-
-    def fit(self, corpus):
-        self.__corpus = corpus
+    def fit(self, raw_corpus):
+        self.__corpus__ = raw_corpus
         return self
 
     def transform(self):
         """output embedding of shape [vocal_size, n_dimensions]"""
-        return np.array(
-                [self._nlp(l).vector for l in self._corpus]).reshape(-1, 300)
+        # !!! investigate how resample word dimenssions
+        embeddings = [l.vector for l in self._corpus]
+        return np.array(embeddings).reshape(-1, 300)
