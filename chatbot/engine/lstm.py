caceb4bacf250fc3446a98fa506caa4ea44a669d
Ming Li <ming.george.li@gmail.com>
2017-06-16 17:54:26 +0100

    join data pipeline to graph and remove data placeholders

--- chatbot/engine/lstm.py
+++ chatbot/engine/lstm.py
@@ -28,18 +28,19 @@ from . import corpus, labels
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 
+def resample(docs, labels, sample_size):
+    unique_labels = np.unique(labels)
+    indice_array = list()
+    for label in unique_labels:
+        indice_label = np.where(labels == label)[0].tolist()
+        indice_array.extend(random.choices(indice_label, k=sample_size))
+    return (docs[indice_array], labels[indice_array])
+
+
 def flatten_split_resample(encoded_corpuses, encoded_labels,
                            valid_ratio=.2,
                            sample_size=1000):
     """break documents into sentences and augment, and one-hot encode labels"""
-
-    def resample(docs, labels, sample_size=sample_size):
-        unique_labels = np.unique(labels)
-        indice_array = list()
-        for label in unique_labels:
-            indice_label = np.where(labels == label)[0].tolist()
-            indice_array.extend(random.choices(indice_label, k=sample_size))
-        return (docs[indice_array], labels[indice_array])
 
     flattened_docs = list()
     flattened_labels = list()
@@ -56,7 +57,8 @@ def flatten_split_resample(encoded_corpuses, encoded_labels,
                                          test_size=valid_ratio,
                                          stratify=ravelled_labels)
 
-    return resample(X_train, y_train), resample(X_test, y_test)
+    return resample(X_train, y_train, sample_size),\
+        resample(X_test, y_test, sample_size)
 
 
 def size(sequence):
@@ -136,31 +138,26 @@ def batch_generator(sent_queue, label_queue, batch_size=None, threads=4):
 
 
 @multithreading
-def train(n, x, y_, train_sent_batch, train_label_batch, valid_sent_batch,
-          valid_label_batch, keep_prob, optimiser, metric, loss, verbose):
+def train(n, is_train, optimiser, metric, loss, verbose):
 
-    for global_step in tqdm(range(n), miniters=1):
-        train_sent, train_label = \
-                    sess.run(fetches=[train_sent_batch, train_label_batch])
+    for global_step in tqdm(range(n), miniters=1, disable=verbose):
         _, train_accuracy, train_loss = \
-            sess.run(fetches=[optimiser, metric, loss],
-                     feed_dict={x: train_sent, y_: train_label, keep_prob: .8})
+            sess.run([optimiser, metric, loss], feed_dict={is_train: True})
 
         if verbose:
             print("step {0} of {3}, train accuracy: {1:.4f} log loss: {2:.4f}"
                   .format(global_step, train_accuracy, train_loss, n))
 
-        if global_step and global_step % 10 == 0:
-            valid_sent, valid_label = \
-                    sess.run(fetches=[valid_sent_batch, valid_label_batch])
-            valid_accuracy, valid_loss = sess.run(
-                    fetches=[metric, loss],
-                    feed_dict={x: valid_sent, y_: valid_label, keep_prob: 1})
+        if global_step and global_step % 100 == 0:
+            valid_accuracy, valid_loss = sess.run(fetches=[metric, loss])
 
-            if verbose:
-                print("step {0} of {3}, valid accuracy: {1:.4f} "
-                      "log loss: {2:.4f}".format(global_step, valid_accuracy,
-                                                 valid_loss, n))
+            print("step {0} of {3}, valid accuracy: {1:.4f} "
+                  "log loss: {2:.4f}".format(global_step, valid_accuracy,
+                                             valid_loss, n))
+
+    print("steps {0} of {0}, train accuray: {1:.4f}, valid accuracy {2:.4f} "
+          "log loss: {3:.4f}".format(n, valid_accuracy, train_accuracy,
+                                     train_loss))
 
 
 N_DOC = 20
@@ -182,14 +179,23 @@ encoded_labels, classes = l_encoder.transform(labels), l_encoder.classes_
 embedding_matrix = corpus_encoder.vectorize()
 embed_shape = embedding_matrix.shape
 
-x = tf.placeholder(dtype=tf.int32, shape=(None, STEP_SIZE), name='feature')
-y_ = tf.placeholder(dtype=tf.uint8, shape=(None, N_CLASS), name='label')
-v = tf.placeholder(dtype=tf.float32, shape=(None, 300), name='vectors')
-keep_prob = tf.placeholder(dtype=tf.float32, name='keep_rate')
+data = flatten_split_resample(encoded_corpus, encoded_labels)
+train_sent, train_label = batch_generator(*enqueue(*data[0]), BATCH_SIZE)
+valid_sent, valid_label = batch_generator(*enqueue(*data[1]), BATCH_SIZE)
 
-embeddings = tf.get_variable(name='embedd', shape=embed_shape, trainable=False)
+query = tf.placeholder_with_default(input=valid_sent,
+                                    shape=[None, STEP_SIZE],
+                                    name='query')
+embeddings = tf.placeholder_with_default(input=embedding_matrix,
+                                         shape=[None, embed_shape[1]],
+                                         name='embeddings')
+is_train = tf.placeholder_with_default(input=tf.constant(False), shape=[], name='is_train_or_valid')
 
-rnn_inputs = tf.nn.embedding_lookup(embeddings, x)
+feature_feed = tf.cond(is_train, lambda: train_sent, lambda: query)
+label_feed = tf.cond(is_train, lambda: train_label, lambda: valid_label)
+keep_prob = tf.cond(is_train, lambda: tf.constant(.5), lambda: tf.constant(1.))
+
+rnn_inputs = tf.nn.embedding_lookup(embeddings, feature_feed)
 
 with tf.variable_scope('softmax'):
     W_softmax = tf.get_variable(
@@ -215,43 +221,39 @@ last = find_last(outputs, sent_length)
 logits = tf.matmul(last, W_softmax) + b_softmax
 # logits in shape [BATCH_SIZE, N_CLASS]
 cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=logits,
-                                                        labels=y_)
+                                                        labels=label_feed)
 # softmax cross entropy in shape [BATCH_SIZE, ]
 # TODO mask padded loss to accelerate training
 loss = tf.reduce_mean(cross_entropy)
 train_step = tf.train.RMSPropOptimizer(1e-3).minimize(loss)
 
 probs = tf.nn.softmax(logits)
-correct = tf.equal(tf.argmax(probs, 1), tf.argmax(y_, 1))
+correct = tf.equal(tf.argmax(probs, 1), tf.argmax(label_feed, 1))
 accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
 
 init = tf.global_variables_initializer()
-init_embedd = tf.assign(embeddings, v, validate_shape=False)
 
 sess = tf.Session()
 sess.run(init)
-sess.run(init_embedd, feed_dict={v: embedding_matrix})
+sess.graph.finalize()
 
 with sess.as_default(), tf.device('/cpu:0'):
-    data = flatten_split_resample(encoded_corpus, encoded_labels,
-                                  valid_ratio=.2, sample_size=1000)
-    train_sent_batch, train_label_batch = \
-        batch_generator(*enqueue(*data[0]), BATCH_SIZE)
-    valid_sent_batch, valid_label_batch = \
-        batch_generator(*enqueue(*data[1]), BATCH_SIZE)
-    train(300, x, y_, train_sent_batch, train_label_batch, valid_sent_batch,
-          valid_label_batch, keep_prob, train_step, accuracy, loss, False)
+    train(5000, is_train, train_step, accuracy, loss, True)
 
 
-def inference(query, sess, encoder=corpus_encoder, classes=classes, limit=5,
-              decision_boundary=.85):
+def inference(question, sess, encoder=corpus_encoder, classes=classes,
+              query=query, embeddings=embeddings, limit=5, decision_boundary=.85):
     """produce probabilities of most probable topic"""
-    encoder, original_pad_length = encoder.fit([query]), encoder.pad_length
+
+    encoder, original_pad_length = encoder.fit([question]), encoder.pad_length
     encoded_query = encoder.encode(pad_length=original_pad_length)
     encoded_query = np.array(encoded_query).reshape(-1, original_pad_length)
-    sess.run(fetches=init_embedd, feed_dict={v: encoder.vectorize()})
+    embedded_query = encoder.vectorize()
     probabilities = sess.run(fetches=probs,
-                             feed_dict={x: encoded_query, keep_prob: 1})
+                             feed_dict={query: encoded_query,
+                                        embeddings: embedded_query})
+
     class_prob = np.mean(probabilities, axis=0).tolist()
     samples = [(class_, class_prob[k]) for k, class_ in enumerate(classes)]
+
     return feed_conversation(samples, limit, decision_boundary)
